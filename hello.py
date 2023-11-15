76fecb43477f88be6ac6b09f5f0ca1f4416fba67
bennie-aranda <92606568+bennie-aranda@users.noreply.github.com>
2023-11-15 07:56:35 +0000

    python code update

--- hello.py
+++ hello.py
@@ -1,1 +1,20 @@
-print ( " hello ")
+def main():
+    while True:
+        print("choose a number?")
+        x = int(input())
+
+        if x > 2:
+            print(x, " is greater than 2!")
+
+        elif x < 2: 
+            print(x, " is less than 2!")
+        
+        else: 
+            print(x, " is equal to ", x)
+
+        restart = input("Do you want to run the program again? (y/n)")
+        if restart.lower() != 'y':
+            break
+     
+if __name__ == "__main__":
+    main()   
